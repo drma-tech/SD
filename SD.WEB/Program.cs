754d91bd1ff997b3117fe34989e5698b2aa43f37
Dhiogo Acioli <dhiogoacioli@outlook.com>
2022-11-07 23:28:34 -0300

    static build 1.1

--- SD.WEB/Program.cs
+++ SD.WEB/Program.cs
@@ -1,16 +1,24 @@
 using AzureStaticWebApps.Blazor.Authentication;
+using Blazored.SessionStorage;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using BlazorPro.BlazorSize;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SD.WEB;
+using SD.WEB.Modules.List.Core.TMDB;
+using System.Globalization;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-builder.Services.ConfigureComponents();
+ConfigureComponents(builder.Services);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.ConfigureServices(builder.HostEnvironment.BaseAddress);
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 builder.Services.AddLogging(logging =>
 {
@@ -19,6 +27,54 @@ builder.Services.AddLogging(logging =>
 
 var hostBuilder = builder.Build();
 
-hostBuilder.ConfigureCulture();
+ConfigureCulture(hostBuilder);
 
 await hostBuilder.RunAsync();
+
+static void ConfigureComponents(IServiceCollection collection)
+{
+    collection
+        .AddBlazorise(options => options.Immediate = true)
+        .AddBootstrapProviders()
+        .AddFontAwesomeIcons();
+
+    collection.AddBlazoredSessionStorage(config => config.JsonSerializerOptions.WriteIndented = true);
+
+    collection.AddPWAUpdater();
+    collection.AddMediaQueryService();
+
+    collection.AddScoped<Settings>();
+}
+
+static void ConfigureServices(IServiceCollection collection, string baseAddress)
+{
+    collection
+        .AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) })
+        .AddStaticWebAppsAuthentication();
+
+    collection.AddScoped<ListService>();
+    collection.AddScoped<MediaDetailService>();
+}
+
+static void ConfigureCulture(WebAssemblyHost host)
+{
+    CultureInfo culture;
+    var session = host.Services.GetRequiredService<ISyncSessionStorageService>();
+    var sett = session.GetItem<Settings>("Settings");
+
+    if (sett != null)
+    {
+        culture = new CultureInfo(sett.Language.GetName(false) ?? "en-US");
+    }
+    else
+    {
+        culture = CultureInfo.CurrentCulture;
+
+        //save the new settings
+        sett = new Settings(session);
+        session.SetItem("Settings", sett);
+    }
+
+    CultureInfo.DefaultThreadCurrentCulture = culture;
+    CultureInfo.DefaultThreadCurrentUICulture = culture;
+}
