5a8193cd7959864747472e1f3f446a2aca86e9f4
Dhiogo Acioli <dhiogoacioli@outlook.com>
2022-12-27 22:34:51 -0300

    BlazorWasmPrerenderingMode

--- SD.WEB/Program.cs
+++ SD.WEB/Program.cs
@@ -18,8 +18,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
 
 await builder.Build().RunAsync();
 
