f472d6dea918442f8c3f12e40633cf2a6115f2a7
Dhiogo Acioli <dhiogoacioli@outlook.com>
2023-05-07 12:13:16 -0400

    nuget updates / imdb fix

--- SD.Shared/Helper/EnumHelper.cs
+++ SD.Shared/Helper/EnumHelper.cs
@@ -18,24 +18,22 @@
             {
                 var attr = val.GetCustomAttribute(translate);
 
-                yield return new EnumObject(Convert.ToInt32(val), val, attr?.Name, attr?.Description, attr?.Group);
+                yield return new EnumObject(val, attr?.Name, attr?.Description, attr?.Group);
             }
         }
     }
 
     public class EnumObject
     {
-        public EnumObject(int Value, object ValueObject, string? Name, string? Description, string? Group)
+        public EnumObject(Enum Value, string? Name, string? Description, string? Group)
         {
             this.Value = Value;
-            this.ValueObject = ValueObject;
             this.Name = Name;
             this.Description = Description;
             this.Group = Group;
         }
 
-        public int Value { get; set; }
-        public object ValueObject { get; set; }
+        public Enum Value { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? Group { get; set; }
