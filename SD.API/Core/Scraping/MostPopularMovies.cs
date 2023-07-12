60dcbfccdbd0828d8de86b86dcca9736b5b883c7
Dhiogo Acioli <dhiogoacioli@outlook.com>
2023-07-12 20:37:21 -0300

    imdb popular movies - still old page

--- SD.API/Core/Scraping/MostPopularMovies.cs
+++ SD.API/Core/Scraping/MostPopularMovies.cs
@@ -38,28 +38,28 @@ namespace SD.API.Core.Scraping
 
             doc.LoadHtml(html);
 
-            var ul = doc.DocumentNode.SelectNodes("//ul[starts-with(@class,'ipc-metadata-list')]").FirstOrDefault();
+            var ul = doc.DocumentNode.SelectNodes("//ul[starts-with(@class,'ipc-metadata-list')]")?.FirstOrDefault();
 
             if (ul != null)
             {
                 foreach (var node in ul.ChildNodes.Take(40))
                 {
-                    var rank = node.SelectNodes($"div[2]/div/div/div[1]").Single().InnerText;
-                    var imageRank = node.SelectNodes($"div[2]/div/div/div[1]/span/svg").Single().ChildAttributes("id").First().Value;
+                    var rank = node.SelectNodes($"div[2]/div/div/div[1]").FirstOrDefault()?.InnerText;
+                    var imageRank = node.SelectNodes($"div[2]/div/div/div[1]/span/svg").FirstOrDefault()?.ChildAttributes("id").First().Value;
                     var rankRegex = Regex.Match(rank, "(?<=\\()([0-9]+)(?=\\))");
-                    var id = node.SelectSingleNode($"div[1]/div/a").ChildAttributes("href").First().Value;
+                    var id = node.SelectSingleNode($"div[1]/div/a").ChildAttributes("href").FirstOrDefault()?.Value;
                     var idRegex = Regex.Match(id, "(?<=\\/tt)(\\w*)(?=\\/)");
-                    var year = node.SelectNodes($"div[2]/div/div/div[3]/span[1]").Single().InnerText.Trim().Split("–")[0];
+                    var year = node.SelectNodes($"div[2]/div/div/div[3]/span[1]").FirstOrDefault()?.InnerText.Trim().Split("–")[0];
                     _ = int.TryParse(year, out int year_fix);
 
                     var item = new MostPopularDataDetail
                     {
                         Id = $"tt{idRegex.Value}",
                         RankUpDown = imageRank == "iconContext-dash" ? "0" : imageRank.Contains("-up") ? $"+{rankRegex.Value}" : $"-{rankRegex.Value}",
-                        Title = node.SelectNodes($"div[2]/div/div/div[2]/a/h3/text()").Single().InnerText,
+                        Title = node.SelectNodes($"div[2]/div/div/div[2]/a/h3/text()").FirstOrDefault()?.InnerText,
                         Year = year_fix == 0 ? "" : year_fix.ToString(),
                         Image = node.SelectSingleNode($"div[1]/div/div[2]/img").ChildAttributes("src").First().Value,
-                        IMDbRating = node.SelectNodes($"div[2]/div/div/span/div/span").Single().InnerText
+                        IMDbRating = node.SelectNodes($"div[2]/div/div/span/div/span").FirstOrDefault()?.InnerText
                     };
 
                     data.Items.Add(item);
