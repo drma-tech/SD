04ec78344764abdcd86e360ebe636173b13769fe
Dhiogo Acioli <dhiogoacioli@outlook.com>
2023-07-20 09:06:19 -0300

    popular movies rating fix

--- SD.API/Core/Scraping/MostPopularMovies.cs
+++ SD.API/Core/Scraping/MostPopularMovies.cs
@@ -44,22 +44,22 @@ namespace SD.API.Core.Scraping
             {
                 foreach (var node in ul.ChildNodes.Take(40))
                 {
-                    var rank = node.SelectNodes($"div[2]/div/div/div[1]").FirstOrDefault()?.InnerText;
-                    var imageRank = node.SelectNodes($"div[2]/div/div/div[1]/span/svg").FirstOrDefault()?.ChildAttributes("class").FirstOrDefault()?.Value;
+                    var rank = node.SelectNodes($"div[2]/div/div/div[1]")?.FirstOrDefault()?.InnerText;
+                    var imageRank = node.SelectNodes($"div[2]/div/div/div[1]/span/svg")?.FirstOrDefault()?.ChildAttributes("class").FirstOrDefault()?.Value;
                     var rankRegex = Regex.Match(rank, "(?<=\\()([0-9]+)(?=\\))");
-                    var id = node.SelectSingleNode($"div[1]/div/a").ChildAttributes("href").FirstOrDefault()?.Value;
+                    var id = node.SelectNodes($"div[1]/div/a")?.FirstOrDefault()?.ChildAttributes("href").FirstOrDefault()?.Value;
                     var idRegex = Regex.Match(id, "(?<=\\/tt)(\\w*)(?=\\/)");
-                    var year = node.SelectNodes($"div[2]/div/div/div[3]/span[1]").FirstOrDefault()?.InnerText.Trim().Split("–")[0];
+                    var year = node.SelectNodes($"div[2]/div/div/div[3]/span[1]")?.FirstOrDefault()?.InnerText.Trim().Split("–")[0];
                     _ = int.TryParse(year, out int year_fix);
 
                     var item = new MostPopularDataDetail
                     {
                         Id = $"tt{idRegex.Value}",
                         RankUpDown = imageRank.Contains("-flat") ? "0" : imageRank.Contains("-up") ? $"+{rankRegex.Value}" : $"-{rankRegex.Value}",
-                        Title = node.SelectNodes($"div[2]/div/div/div[2]/a/h3/text()").FirstOrDefault()?.InnerText,
+                        Title = node.SelectNodes($"div[2]/div/div/div[2]/a/h3/text()")?.FirstOrDefault()?.InnerText,
                         Year = year_fix == 0 ? "" : year_fix.ToString(),
-                        Image = node.SelectSingleNode($"div[1]/div/div[2]/img").ChildAttributes("src").FirstOrDefault()?.Value,
-                        IMDbRating = node.SelectNodes($"div[2]/div/div/span/div/span").FirstOrDefault()?.InnerText
+                        Image = node.SelectNodes($"div[1]/div/div[2]/img")?.FirstOrDefault()?.ChildAttributes("src").FirstOrDefault()?.Value,
+                        IMDbRating = node.SelectNodes($"div[2]/div/div/span/div/span")?.FirstOrDefault()?.InnerText
                     };
 
                     data.Items.Add(item);
