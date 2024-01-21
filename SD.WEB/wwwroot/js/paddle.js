e17be1827c008faa0cef469b3fa24f540b7483ee
Dhiogo Acioli <dhiogoacioli@outlook.com>
2024-01-21 17:42:48 -0500

    profile default payment link

--- SD.WEB/wwwroot/js/paddle.js
+++ SD.WEB/wwwroot/js/paddle.js
@@ -44,6 +44,7 @@ async function getPlans(price_standard_month, price_standard_year, price_premium
         })
         .catch((error) => {
             console.error(error);
+            DotNet.invokeMethodAsync('SD.WEB', 'ShowError', error.error.detail);
         });
 
     return list;
