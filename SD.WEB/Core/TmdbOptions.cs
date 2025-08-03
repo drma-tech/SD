08bee2909947923cd6add6849c48de97d542a04c
Dhiogo Acioli <dhiogoacioli@outlook.com>
2025-08-04 00:32:25 +0700

    new api key / tablets screenshots / relocate screenshots outisde wwwroot

--- SD.WEB/Core/TmdbOptions.cs
+++ SD.WEB/Core/TmdbOptions.cs
@@ -9,7 +9,7 @@ public static class TmdbOptions
 
     public const string BaseUri = "https://api.themoviedb.org/3/";
     public const string BaseUriNew = "https://api.themoviedb.org/4/";
-    public const string ApiKey = "4e0dee3ee7786c86df1f434849570b55";
+    public const string ApiKey = "3c3f4a904d959f4dbb0c8adda1be9433";
     public const string SmallPosterPath = "https://image.tmdb.org/t/p/w185/";
     public const string LargePosterPath = "https://image.tmdb.org/t/p/w300/";
     public const string OriginalPosterPath = "https://image.tmdb.org/t/p/original/";
