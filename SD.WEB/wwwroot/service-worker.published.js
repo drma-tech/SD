e07d8f9f596c7fa619f6bd9e87dfd8ded22b70ba
Dhiogo Acioli <dhiogoacioli@outlook.com>
2025-08-09 17:10:01 +0700

    ...

--- SD.WEB/wwwroot/service-worker.published.js
+++ SD.WEB/wwwroot/service-worker.published.js
@@ -18,6 +18,14 @@ self.addEventListener("fetch", event => {
     event.respondWith(onFetch(event));
 });
 self.addEventListener("message", event => {
+    try {
+        if (event.origin) {
+            const url = new URL(event.origin);
+            if (!url.hostname.endsWith(".streamingdiscovery.com")) return;
+        }
+        else { return; }
+    } catch { return; }
+
     if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
 });
 
