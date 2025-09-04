5ac37469d72ee7d6e3bd7b5f10f638059ff34b0c
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-09-04 14:15:40 +0700

    delay subs / removing paddle logo

--- SD.API/Functions/PaddleFunction.cs
+++ SD.API/Functions/PaddleFunction.cs
@@ -52,6 +52,8 @@ public class PaddleFunction(CosmosRepository repo, IHttpClientFactory factory)
             var body = await req.GetPublicBody<RootEvent>(cancellationToken) ?? throw new UnhandledException("body null");
             if (body.data == null) throw new UnhandledException("body.data null");
 
+            await Task.Delay(300, cancellationToken); //wait for user be updated in cosmos
+
             var result = await repo.Query<AuthPrincipal>(x => x.AuthPaddle != null && x.AuthPaddle.CustomerId == body.data.customer_id, DocumentType.Principal, cancellationToken) ??
                 throw new UnhandledException("AuthPrincipal null");
             var client = result.FirstOrDefault() ?? throw new UnhandledException($"client null - customer_id:{body.data.customer_id}");
