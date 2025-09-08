898ba1cf22370a23a2ee335fb06e481ddecf48cf
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-09-08 19:42:38 +0700

    adsense min width / subscription new providers (coming soon)

---
+++ SD.Shared/Enums/PaymentProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace SD.Shared.Enums
+{
+    public enum PaymentProvider
+    {
+        [Custom(Name = "Paddle")]
+        Paddle = 1,
+
+        [Custom(Name = "Microsoft Store")]
+        Microsoft = 2,
+
+        [Custom(Name = "Google Play")]
+        Google = 3,
+
+        [Custom(Name = "App Store")]
+        Apple = 4
+    }
+}
