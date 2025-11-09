ac3b0e88484dc4d605e709bbda3483d382944b50
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-11-09 17:34:57 +0700

    Reapply "trying firebase auth"
    This reverts commit 9502c72f0b5b1b441c24a7083334ae675e9a84e2.

---
+++ SD.WEB/Core/Service/FirebaseAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+using SD.WEB.Layout;
+
+namespace SD.WEB.Core.Service
+{
+    public class FirebaseAuthService(IJSRuntime js)
+    {
+        public async Task<string?> SignInAsync() => await js.InvokeAsync<string?>("firebaseAuth.signIn");
+
+        public async Task SignOutAsync() => await js.InvokeVoidAsync("firebaseAuth.signOut");
+
+        public async Task SubscribeToAuthChanges(DotNetObjectReference<MainLayout> dotNetRef) => await js.InvokeVoidAsync("firebaseAuth.onAuthChanged", dotNetRef);
+    }
+}
