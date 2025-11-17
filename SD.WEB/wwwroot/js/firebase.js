9b1fdda2a1eab1c37467985177c328d9d41092eb
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-11-17 12:58:18 +0700

    new auth domain 3

--- SD.WEB/wwwroot/js/firebase.js
+++ SD.WEB/wwwroot/js/firebase.js
@@ -6,7 +6,7 @@
 
 const firebaseConfig = {
     apiKey: "AIzaSyDj5LpsT7-bra4hvuvb5E_BPSlD7Wr29nQ",
-    authDomain: "firebase.streamingdiscovery.com",
+    authDomain: "auth.streamingdiscovery.com",
     projectId: "streaming-discovery-4c483",
     storageBucket: "streaming-discovery-4c483.firebasestorage.app",
     messagingSenderId: "394152837411",
