3cb4a95179bc1089dea1de3a552cfab7ebd61f9e
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2025-11-18 19:03:32 +0700

    Revert "measuring startup timing 2"
    This reverts commit d0d856c374ae7c0e9520c927315fcbfc2b1ea1ad.

--- SD.API/Program.cs
+++ SD.API/Program.cs
@@ -53,27 +53,7 @@ var app = new HostBuilder()
     .ConfigureServices(ConfigureServices) //125
     .Build();
 
-// measure host start (this is the Azure Functions host initialization you mentioned)
-var swHostStart = Stopwatch.StartNew();
-try
-{
-    await app.StartAsync();
-    swHostStart.Stop();
-
-    var logger = app.Services.GetService<ILogger<Program>>();
-    if (logger != null)
-        logger.LogInformation("Host started in {Elapsed}", swHostStart.Elapsed);
-    else
-        StartupLogBuffer.Enqueue($"Host started in {swHostStart.Elapsed}");
-}
-catch (Exception ex)
-{
-    swHostStart.Stop();
-    StartupLogBuffer.Enqueue($"Host failed to start after {swHostStart.Elapsed}: {ex.Message}");
-    throw;
-}
-
-await app.WaitForShutdownAsync();
+await app.RunAsync(); //1442
 
 static void ConfigureServices(IServiceCollection services)
 {
