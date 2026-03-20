114dc2c0c400509b3beb2f41b86d58c1801b532d
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-03-20 16:14:28 -0300

    ...

--- SD.WEB/Modules/Collections/Core/EnumLists.cs
+++ SD.WEB/Modules/Collections/Core/EnumLists.cs
@@ -2,18 +2,18 @@
 
 public enum EnumLists
 {
-    [Custom(Name = "Most Expected Movies of 2025", Group = "")]
+    [Custom(Name = "Most Expected Movies of 2025")]
     ExpectedMovieOf2025 = 8300898,
 
-    [Custom(Name = "Most Expected Movies of 2026", Group = "")]
+    [Custom(Name = "Most Expected Movies of 2026")]
     ExpectedMovieOf2026 = 8544544,
 
-    [Custom(Name = "Best awards of the year", Group = "")]
+    [Custom(Name = "Best awards of the year")]
     AwardsOfTheYear = 8498534,
 
-    [Custom(Name = "Editor’s Choice", Group = "")]
+    [Custom(Name = "Editor’s Choice")]
     CertifiedStreamingDiscoveryMovies = 8498673,
 
-    [Custom(Name = "Editor’s Choice", Group = "")]
+    [Custom(Name = "Editor’s Choice")]
     CertifiedStreamingDiscoveryShows = 8498675,
 }
