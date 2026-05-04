88e4651b8f57f307c0f61a0aa0fb68a736958203
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-05-04 17:39:08 -0300

    ads block detector - msg

--- SD.WEB/Resources/GlobalTranslations.Designer.cs
+++ SD.WEB/Resources/GlobalTranslations.Designer.cs
@@ -61,6 +61,15 @@ namespace SD.WEB.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to AdBlocker detected. Please consider whitelisting our app to support us. Thank you!.
+        /// </summary>
+        public static string AdBlockerDetected {
+            get {
+                return ResourceManager.GetString("AdBlockerDetected", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to App Language.
         /// </summary>
         public static string AppLanguage {
