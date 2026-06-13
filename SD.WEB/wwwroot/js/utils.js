58420594f995716d739152d44dee2b3e97d37ec3
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-06-13 09:44:17 -0300

    detect ublock lite

--- SD.WEB/wwwroot/js/utils.js
+++ SD.WEB/wwwroot/js/utils.js
@@ -262,7 +262,7 @@ export const environment = {
 
             document.head.appendChild(script);
 
-            setTimeout(() => finish(false), 3000);
+            setTimeout(() => finish(false), 10000);
         });
     },
     async isAdBlocked() {
@@ -304,6 +304,23 @@ export const environment = {
             if (!fundingchoicesmessages) {
                 return true;
             }
+        }
+
+        //other blockers block other urls
+        const sentry = await environment.testUrl(
+            'https://js.sentry-cdn.com/94ae67eb3fb0bc82327607ddd9d6aebb.min.js'
+        );
+
+        if (!sentry) {
+            return true;
+        }
+
+        const clarity = await environment.testUrl(
+            'https://www.clarity.ms/tag/r2iwqdpwtv'
+        );
+
+        if (!clarity) {
+            return true;
         }
 
         Sentry.captureMessage("ad blocked - Ads failed but no blocker detected", "error");
