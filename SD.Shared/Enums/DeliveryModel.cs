d56dcba7ab37f625c99e167fa9d5b2e6b913d6e3
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-07-16 16:11:04 -0300

    providers updated / premium delivery removed

--- SD.Shared/Enums/DeliveryModel.cs
+++ SD.Shared/Enums/DeliveryModel.cs
@@ -2,18 +2,33 @@
 
 public enum DeliveryModel
 {
+    /// <summary>
+    /// Free
+    /// </summary>
     [FieldSettings("FREEName", Description = "FREEDescription", ResourceType = typeof(Translations.Enum.DeliveryModel))]
     FREE = 0,
 
+    /// <summary>
+    /// Advertising
+    /// </summary>
     [FieldSettings("AVODName", Description = "AVODDescription", ResourceType = typeof(Translations.Enum.DeliveryModel))]
     AVOD = 1,
 
+    /// <summary>
+    /// Subscription
+    /// </summary>
     [FieldSettings("SVODName", Description = "SVODDescription", ResourceType = typeof(Translations.Enum.DeliveryModel))]
     SVOD = 2,
 
+    /// <summary>
+    /// Transaction
+    /// </summary>
     [FieldSettings("TVODName", Description = "TVODDescription", ResourceType = typeof(Translations.Enum.DeliveryModel))]
     TVOD = 3,
 
-    [FieldSettings("PVODName", Description = "PVODDescription", ResourceType = typeof(Translations.Enum.DeliveryModel))]
-    PVOD = 4
+    ///// <summary>
+    ///// Premium
+    ///// </summary>
+    //[FieldSettings("PVODName", Description = "PVODDescription", ResourceType = typeof(Translations.Enum.DeliveryModel))]
+    //PVOD = 4
 }
