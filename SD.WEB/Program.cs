bb3cf45689c99495277b9d03d92f8ae7ada2a23d
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-08-07 18:41:28 -0300

    ...

--- SD.WEB/Program.cs
+++ SD.WEB/Program.cs
@@ -97,12 +97,6 @@ static void ConfigureServices(IServiceCollection collection, string baseAddress,
     {
         var builder = new UriBuilder(baseAddress);
 
-        //force apex domain
-        if (builder.Host.StartsWith("www.", StringComparison.OrdinalIgnoreCase))
-        {
-            builder.Host = builder.Host[4..];
-        }
-
         webUri = builder.Uri;
         apiUri = new Uri(webUri, "api/");
     }
