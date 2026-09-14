993c0ca4ce90b5b92b884ccc1a728ccc9634e1b7
ASUS-G16-2025\Dhiogo Acioli <dhiogoacioli@gmail.com>
2026-09-14 09:44:40 -0300

    remove middle ads

--- SD.WEB/Shared/Advertising.razor.cs
+++ SD.WEB/Shared/Advertising.razor.cs
@@ -13,7 +13,7 @@ namespace SD.WEB.Shared
     public enum AdSlot
     {
         Header,
-        Middle,
+        //Middle,
         Footer,
     }
 
@@ -61,7 +61,7 @@ namespace SD.WEB.Shared
         private static readonly Dictionary<AdSlot, string> _adsenseSlots = new()
         {
             { AdSlot.Header, "7737592628" },
-            { AdSlot.Middle, "6324586995" },
+            //{ AdSlot.Middle, "6324586995" },
             { AdSlot.Footer, "4604311219" },
         };
 
@@ -70,7 +70,7 @@ namespace SD.WEB.Shared
         private static readonly Dictionary<AdSlot, string> _yandexSlots = new()
         {
             { AdSlot.Header, "R-A-19337518-2" },
-            { AdSlot.Middle, "R-A-19337518-1" },
+            //{ AdSlot.Middle, "R-A-19337518-1" },
             { AdSlot.Footer, "R-A-19337518-3" },
         };
 
